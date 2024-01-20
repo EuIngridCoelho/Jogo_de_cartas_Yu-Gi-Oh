326bfb553f0ef2d254f5d44000e488cf0f118f37
EuIngridSouza <ingridcoelhodeabreu@gmail.com>
2024-01-20 18:49:00 -0300

    Implementando a lógica de setar as cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,19 +1,19 @@
 const state = {
-    score:{
+    score: {
         playerScore: 0,
-        computerScore:0,
+        computerScore: 0,
         scoreBox: document.getElementById("score_points"),
     },
-    cardSprites:{
+    cardSprites: {
         avatar: document.getElementById("card-image",),
         name: document.getElementById("card-name"),
         type: document.getElementById("card-type")
     },
-    fieldCards:{
+    fieldCards: {
         player: document.getElementById("player-field-card"),
         computer: document.getElementById("computer-field-card"),
     },
-    actions:{
+    actions: {
         button: document.getElementById("next-duel"),
     },
 };
@@ -27,38 +27,66 @@ const pathImages = ".src/assets/icons/"
 
 const cardData = [
     {
-        id:0,
+        id: 0,
         name: "Blue Eyes White Dragon",
         type: "Paper",
         img: `${pathImages}dragon.png`,
-        WinOf:[1],
-        LoseOf:[2],
+        WinOf: [1],
+        LoseOf: [2],
     },
     {
-        id:1,
+        id: 1,
         name: "Dark Magician",
         type: "Rock",
         img: `${pathImages}magician.png`,
-        WinOf:[2],
-        LoseOf:[0],
+        WinOf: [2],
+        LoseOf: [0],
     },
     {
-        id:2,
+        id: 2,
         name: "Exodia",
         type: "Scissors",
         img: `${pathImages}exodia.png`,
-        WinOf:[0],
-        LoseOf:[1],
+        WinOf: [0],
+        LoseOf: [1],
     }
 ]
 
-async function drawCards(cardNumbers, fieldSide){
-    for(let i = 0; i < cardNumbers; i++){
+
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length)
+
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(idCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", ".src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", idCard);
+    cardImage.classList.add("card");
+
+    if (fieldSide === playerSides.player1) {
+        cardImage.addEventListener("click", () => {
+            setCardsField(cardImage.getAttribute("data-id"));
+        })
+    }
+
+    cardImage.addEventListener("mouseover", ()=>{
+        drawSelectCard(idCard);
+    });
+
+    return cardImage
+
+}
+
+async function drawCards(cardNumbers, fieldSide) {
+    for (let i = 0; i < cardNumbers; i++) {
         const randomIdCard = await getRandomCardId();
         const cardImage = await createCardImage(randomIdCard, fieldSide);
 
         document.getElementById(fieldSide).appendChild(cardImage);
-        
+
     }
 }
 
@@ -68,7 +96,7 @@ async function drawCards(cardNumbers, fieldSide){
 
 
 
-function initialize(){
+function initialize() {
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
 }
