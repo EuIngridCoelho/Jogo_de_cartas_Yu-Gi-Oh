f1dccf7b1f28b4c622af893954c4d83631042c31
EuIngridSouza <ingridcoelhodeabreu@gmail.com>
2024-01-20 20:10:20 -0300

    Implementando efeitos sonoros

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -103,30 +103,31 @@ async function setCardsField(cardId) {
 
 
 async function drawButton(text) {
-    state.actions.button.innerText = text;
+    state.actions.button.innerText = text.toUpperCase();
     state.actions.button.style.display = "block";
 }
 
 
-async function updateScore(){
+async function updateScore() {
     state.score.scoreBox.innerText = `Win: ${state.score.playerScore} | Lose: ${state.score.computerScore}`
 }
 
 async function checkDuelResults(playerCardId, computerCardId) {
-    let duelResults = "Empate";
+    let duelResults = "draw";
     let playerCard = cardData[playerCardId];
 
 
     if (playerCard.WinOf.includes(computerCardId)) {
-        duelResults = "Ganhou!";
+        duelResults = "win";
         state.score.playerScore++;
     }
 
     if (playerCard.LoseOf.includes(computerCardId)) {
-        duelResults = "Perdeu!";
+        duelResults = "lose";
         state.score.computerScore++;
     }
 
+    await playAudio(duelResults);
     return duelResults;
 
 }
@@ -156,7 +157,23 @@ async function drawCards(cardNumbers, fieldSide) {
     }
 }
 
+async function resetDuel() {
+    state.cardSprites.avatar.src = "";
+    state.actions.button.style.display = "none";
 
+    state.fieldCards.player.style.display = "none";
+    state.fieldCards.computer.style.display = "none";
+
+    initialize();
+}
+
+async function playAudio(status) {
+    const audio = new Audio(`./src/assets/audios/${status}.wav`);
+
+    try {
+        audio.play();
+    } catch {}
+}
 
 
 
